b7e8ce9d31ae6370d900e8508190dee2433ed42f
cmdr2 <secondary.cmdr2@gmail.com>
2024-04-07 14:52:02 +0530

    Add logging and set the time intervals based on experimentation

--- pump.py
+++ pump.py
@@ -1,9 +1,26 @@
 import RPi.GPIO as GPIO
 import time
+import logging
 
+# consts
+ONE_SEC = 1  # seconds
+ONE_MIN = 60 * ONE_SEC
+ONE_HOUR = 60 * ONE_MIN
+
+# setup logging
+logging.basicConfig(
+    filename="pump.log",
+    filemode="a",
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+    level=logging.DEBUG,
+)
+
+logging.info("Running pump")
+
+# setup pins
 OUT_PIN = 17
-WAKE_INTERVAL = 5  # seconds
-PUMP_DURATION = 2  # seconds
+WAKE_INTERVAL = 30 * ONE_MIN
+PUMP_DURATION = 39 * ONE_SEC
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -11,12 +28,12 @@ GPIO.setup(OUT_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 try:
     while True:
-        print(time.time(), "Pumping..")
+        logging.info("Pumping..")
         GPIO.output(OUT_PIN, GPIO.HIGH)
         time.sleep(PUMP_DURATION)
         GPIO.output(OUT_PIN, GPIO.LOW)
 
-        print(time.time(), "Sleeping..")
+        logging.info("Sleeping..")
 
         time.sleep(WAKE_INTERVAL)
 except KeyboardInterrupt:
